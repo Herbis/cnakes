42abcf7db5ab46ebc1f7a03c26a2adbbb6d6fd0c
Herberts Markuns <herbis33@gmail.com>
2020-03-11 18:49:03 +0200

    #13 Additional video properties

--- src/main/java/lv/herbis/cnakes/configuration/VideoConfiguration.java
+++ src/main/java/lv/herbis/cnakes/configuration/VideoConfiguration.java
@@ -1,7 +1,10 @@
 package lv.herbis.cnakes.configuration;
 
 public class VideoConfiguration {
+
     private ResolutionConfiguration resolution;
+    private int scale;
+    private Integer monitor;
 
     public ResolutionConfiguration getResolution() {
         return resolution;
@@ -9,5 +12,21 @@ public class VideoConfiguration {
 
     public void setResolution(ResolutionConfiguration resolution) {
         this.resolution = resolution;
+    }
+
+    public int getScale() {
+        return scale;
+    }
+
+    public void setScale(int scale) {
+        this.scale = scale;
+    }
+
+    public Integer getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Integer monitor) {
+        this.monitor = monitor;
     }
 }
