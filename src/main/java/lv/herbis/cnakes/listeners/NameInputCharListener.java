605cabded42d76a425fb06af352b69145f17a200
Herberts Markuns <herbis33@gmail.com>
2021-03-11 12:36:07 +0200

    #61 Remove some equals method logic

--- src/main/java/lv/herbis/cnakes/listeners/NameInputCharListener.java
+++ src/main/java/lv/herbis/cnakes/listeners/NameInputCharListener.java
@@ -3,8 +3,6 @@ package lv.herbis.cnakes.listeners;
 import lv.herbis.cnakes.status.GameStatus;
 import org.lwjgl.glfw.GLFWCharCallback;
 
-import java.util.Objects;
-
 public class NameInputCharListener extends GLFWCharCallback {
 
 	private final GameStatus gameStatus;
@@ -23,21 +21,6 @@ public class NameInputCharListener extends GLFWCharCallback {
 
 	@Override
 	public boolean equals(final Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (getClass() != o.getClass()) {
-			return false;
-		}
-		if (!super.equals(o)) {
-			return false;
-		}
-		final NameInputCharListener that = (NameInputCharListener) o;
-		return Objects.equals(this.gameStatus, that.gameStatus);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(super.hashCode(), this.gameStatus);
+		return this == o;
 	}
 }
